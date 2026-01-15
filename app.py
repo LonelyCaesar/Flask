f5d9dd4342e0ef01c0620c98d285392b695af9a6
Wu Caesar <a0938408716@gmail.com>
2026-01-15 20:34:34 +0800

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+#建立Flask類別的實體(instance)
+app = Flask(__name__)
+
+# 配對網址和執行的函數
+@app.route("/page/<name>")
+def home(name):
+    return render_template("index.html", name=name)
+    
+if __name__ == "__main__":
+	app.run(debug=True)
